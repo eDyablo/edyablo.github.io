7a68304ab6f312dcf6cf488ba1097527d6e5f112
edyablo <eyablonsky@gmail.com>
2018-12-02 20:18:35 +0200

    react only on touch

--- r6n/r6n.js
+++ r6n/r6n.js
@@ -88,8 +88,9 @@ class Display {
   }
 
   addClickListener(listener) {
-    this.canvas.addEventListener('click', listener)
-    this.dom.addEventListener('keypress', listener)
+    //this.canvas.addEventListener('click', listener)
+    this.canvas.addEventListener('touchstart', listener)
+    //this.dom.addEventListener('keypress', listener)
   }
 
   drawMarker() {
